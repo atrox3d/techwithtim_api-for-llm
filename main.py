adf5ecfbfc6c8437d11fb0915b7c6bb1c153bd0f
atrox3d <atrox3d@gmail.com>
2025-02-23 10:08:10 +0100

    add generate endpoint

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 import ollama
 
 app = FastAPI()
+
+@app.post('/generate')
+def generate(
+    prompt:str = Body(embed=True)   # {"prompt": "actual prompt"}
+                                    # without using pydantic model
+):
+    print(f'{prompt = }')
+    response = ollama.chat(
+            model='mistral',
+            messages=[
+                {
+                    'role': 'user',
+                    'content': prompt
+                },
+            ]
+    )
+    return {'response': response.message.content}
 
